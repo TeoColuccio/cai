8117878fcd70b9a56107b48a9243ddc603f94642
Teodoro777 <teodoro.coluccio@gmail.com>
2016-10-31 12:07:44 +0100

    Sostituita randint() con randrange()

--- python/myrandom.py
+++ python/myrandom.py
@@ -1,7 +1,7 @@
 from random import *
 
 def myrandom_beetween(min, max):
-    return randint(min, max)
+    return randrange(min, max)
 
 if __name__ =='__main__':
     print myrandom_beetween(1, 10)
