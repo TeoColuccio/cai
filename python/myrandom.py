f422b980151b6947d4bc7c1483b871921e85e037
Teodoro777 <teodoro.coluccio@gmail.com>
2016-10-31 11:34:52 +0100

    Piccolo update

--- python/myrandom.py
+++ python/myrandom.py
@@ -1,10 +1,10 @@
-import random
+from random import *
 
 def myrandom_init():
-    random.seed()
+    seed()
 
 def myrandom_beetween(min, max):
-    return random.randint(min, max)
+    return randint(min, max)
 
 if __name__ =='__main__':
     print myrandom_beetween(1, 10)
