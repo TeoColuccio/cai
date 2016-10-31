0c90966e34002edaf4dd0037655b4143fdf7e85e
Teodoro777 <teodoro.coluccio@gmail.com>
2016-10-31 12:00:12 +0100

    Caricato main.py

---
+++ python/main.py
@@ -0,0 +1,29 @@
+# Il programma genera 10 moltiplicazioni tra numeri casuali
+# compresi tra 0 e 10. Lo studente dovra' rispondere ed il programma 
+# calcolera' le risposte esatte, se queste sono inferiori al 75%, il
+# programma consiglia allo studente di ripetere.
+
+from cai import *
+
+esatte = 0
+conta = 0
+
+while conta < 10:
+    a = cai_extract()
+    b = cai_extract()
+    prod = a * b
+    
+    print 'Quanto fa la seguente moltiplicazione: ', a, '*', b
+    risposta = input()
+
+    if risposta != prod:
+        print cai_false()
+    else:
+        print cai_true()
+        esatte += 1 
+    conta += 1
+
+if esatte < 7:
+    print 'Please ask your instructor for extra help!'
+else: 
+    print 'Very good'
